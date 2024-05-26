317340f1e7a3258ca9eb84c8af93d4ee7c8af6c6
Emma <31828821+Gemmstone@users.noreply.github.com>
2024-05-25 23:41:46 -0500

    Update script.js

--- docs/Viewer/script.js
+++ docs/Viewer/script.js
@@ -96,8 +96,8 @@ function preventDefaultDrag(event) {
 }
 
 document.onmousemove = (event) => {
-  var x = event.clientX * 100 / window.innerWidth + "%";
-  var y = event.clientY * 100 / window.innerHeight + "%";
+  var x = event.clientX * 100 / document.innerWidth + "%";
+  var y = event.clientY * 100 / document.innerHeight + "%";
   document.getElementById("resultFrame").contentWindow.cursorPosition(x, y);
 }
 
