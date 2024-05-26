01ef281ed5b5451d71d71a8a394542d7e116458c
Emma <31828821+Gemmstone@users.noreply.github.com>
2024-05-25 22:52:10 -0500

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -78,3 +78,14 @@ function markdownParse(url, id) {
     });
   });
 }
+
+function waitForElement(elementId, callBack){
+  window.setTimeout(function(){
+    var element = document.getElementById(elementId);
+    if(element){
+      callBack(elementId, element);
+    }else{
+      waitForElement(elementId, callBack);
+    }
+  },500)
+}
